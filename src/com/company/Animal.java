b708da33ca8a6848443a195fc66ee1b4a1aee106
kristianlive <kristian.shneltser@learnet.se>
2021-01-18 19:16:21 +0100

    Initial commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public abstract class Animal {
+    public String name;
+    public boolean gender;
+    public int health;
+
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Player {
+    public String name;
+    public int money;
+}
